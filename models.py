276f6b3930954c84252cebe0d6b78b74dc0b3c2b
CSchank <schankuc@mcmaster.ca>
2018-05-17 23:54:57 +0000

    non-caching MathJax server working

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class LaTeXExpr(models.Model):
+        expression = models.CharField(max_length=256)
+        create_date = models.DateTimeField('date created')
+        accesses = models.BigIntegerField('number of accesses')
+        svg_file = models.FileField(upload_to='latex_cache')
--- views.py
+++ views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import LaTeXExpr
+import requests
+import json
 
 # Create your views here.
+def renderlatex(request, expr):
+	latexExpr = LaTeXExpr.objects.filter(expression=expr)
+	data = {
+             "format": "TeX",
+            "math": expr,
+            "svg": True,
+            "mml": False,
+            "png": False,
+            "speakText": True,
+            "speakRuleset": "mathspeak",
+            "speakStyle": "default",
+            "ex": 6,
+            "width": 100,
+            "linebreaks": False,
+        }
+	jsondata = json.dumps(data)
+	r = requests.post(url="http://localhost:8003",data=jsondata)
+	svg_data=r.text
+	return HttpResponse(svg_data,content_type="image/svg+xml")
